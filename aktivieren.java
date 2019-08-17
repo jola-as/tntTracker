d4e78cf06174f74cb7d03a32b5725541c1fee4d8
JAB1305 <54218874+JAB1305@users.noreply.github.com>
2019-08-17 22:16:41 +0200

    Add files via upload

---
+++ aktivieren.java
@@ -0,0 +1,15 @@
+package de.jab_1305.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class aktivieren implements CommandExecutor{
+	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
+		System.out.println("Test");
+		return false;
+		
+	}
+
+}
