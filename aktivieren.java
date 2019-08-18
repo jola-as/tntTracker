8386f242775fd9a3d0c74514f4821c90659029d8
JAB1305 <54218874+JAB1305@users.noreply.github.com>
2019-08-18 20:35:23 +0200

    Update aktivieren.java

--- aktivieren.java
+++ aktivieren.java
@@ -1,15 +1,28 @@
 package de.jab_1305.commands;
 
+import java.util.List;
+
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
-public class aktivieren implements CommandExecutor{
-	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
-		System.out.println("Test");
-		return false;
-		
-	}
 
+
+public class aktivieren implements CommandExecutor{
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if(sender instanceof Player ) {
+			Player p = (Player) sender;
+			p.sendMessage("Test");
+			World w = p.getWorld();
+			List<Entity> l =w.getEntities();
+			System.out.println(l);
+		}
+		
+		return false;
+	}
 }
