c022f169d8eb3bb910582d2f78f0fe9c6deab0f2
JAB1305 <54218874+JAB1305@users.noreply.github.com>
2019-08-21 20:40:14 +0200

    Update aktivieren.java

--- aktivieren.java
+++ aktivieren.java
@@ -2,7 +2,7 @@ package de.jab_1305.commands;
 
 import java.util.List;
 
-import org.bukkit.Location;
+
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -21,14 +21,7 @@ public class aktivieren implements CommandExecutor{
 			p.sendMessage("Test");
 			World currentW = p.getWorld();
 			List<Entity> LEntities =currentW.getEntities();
-			System.out.println(LEntities);
-			List<Entity> LTnt = null;
-			for (int i=0; i<LEntities.size(); i++) {
-				if(LEntities.get(i).getName() == "CraftTNTPrimed") {
-					LTnt.add(LEntities.get(i));
-				}
-			}
-			System.out.println(LTnt);
+			p.sendMessage(LEntities.get(3).getName());
 		}
 		return true;
 	}
